3cbc44f9141cbb533b10ead0e244d8834c373c0f
Deki97 <dejan97@libero.it>
2021-11-18 16:28:46 +0100

    scaffolding

---
+++ milestone-2/js/script.js
@@ -0,0 +1,7 @@
+// Consegna:
+// Dati tre array contenenti:
+//  - una lista ordinata di 5 immagini,
+//  - una lista ordinata dei relativi 5 luoghi e
+//  - una lista di 5 news,
+// creare un carosello come nella foto allegata.
+
